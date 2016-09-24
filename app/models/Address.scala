1d4f571ae877281c9eb115bb6706a5c759fb722d
Dorian Geraut <dorian.geraut@telecomancy.net>
2016-09-24 16:41:57 +0200

    progrssion

--- app/models/Address.scala
+++ app/models/Address.scala
@@ -1,30 +1,29 @@
 package models
 
 import play.api.libs.json._
-
+import play.api.libs.functional.syntax._
 /**
  * Created by fred on 17/04/2015.
  */
+ 
 case class Address(street : String, town:String, zipCode:String)
 
 
 object Address{
   //TODO json Format
-  implicit val addressFormat = new Format[Address] {
-      def Writes(address: Address) = {
-          JsObject(Seq(
-              "street" -> JsString(address.street),
-              "town" -> JsString(address.town),
-              "zipCode" -> JsString(address.zipCode)
-          ))
-      }
-    
-    def Reads(jsonAdress: JsValue) = {
-        val street = (jsonAdress \ "street").as[String]
-        val town = (jsonAdress \ "town").as[String]
-        val zipCode = (jsonAdress \ "zipCode").as[String]
-        Address(street,town,zipCode)
-    }
-      
-  }
+  
+  
+  implicit val addressReads: Reads[Address] = (
+      (JsPath \ "street").read[String] and
+      (JsPath \ "town").read[String] and
+      (JsPath \ "zipCode").read[String]
+   )(Address.apply _)
+  
+   implicit val addressWrites = new Writes[Address] {
+       def writes(address: Address) = Json.obj(
+           "street" -> address.street,
+           "town" -> address.town,
+           "zipCode" -> address.zipCode
+           )
+   }
 }
--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,20 +1,26 @@
 package models
 
+import models.Address
+
 import org.joda.time.DateTime
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 /**
  * Created by fred on 17/04/2015.
  */
 
 trait Sex
-case object male extends Sex
-case object female extends Sex
+case object Male extends Sex
+case object Female extends Sex
 
 trait AddressType
-case object personal extends AddressType // Petite correction: Sex devient AddressType
-case object professional extends AddressType // Petite correction: Sex devient AddressType
+case object Personal extends AddressType // Petite correction: Sex devient AddressType
+case object Professional extends AddressType // Petite correction: Sex devient AddressType
 
 
 case class Person(name:String, lastName:String, birthDate:DateTime, sex: Sex, addresses : Map[AddressType, Address] = Map.empty) {
@@ -27,91 +33,82 @@ case class Person(name:String, lastName:String, birthDate:DateTime, sex: Sex, ad
 
 }
 
+
+
 object AddressType{
   //TODO hint : créer le Json Read et Write (ou Format)
-  /*
-    implicit val addressTypeFormat = new Format[AddressType] {
-   def Writes(addressType: AddressType) = addressType match{
-        case personal => val mySeq = Seq(
-            "addressType" -> JsString("personal")
-            )
-        case professional => val mySeq = Seq(
-            "addressType" -> JsString("professional")
-            )
-        JsObject(mySeq)
-   }
-    
-    def Reads(jsonAddressType: JsValue) = {
-        val addressType = (jsonAddressType \ "addressType").as[String]
-        addressType match {
-            case "personal" => personal
-            case "professional" => professional
-        }
+
+    def valueOf(value: String): AddressType = value match{
+        case "professional" => Professional
+        case "personal" => Personal
+        case _ => throw new IllegalArgumentException()
     }
-  }
-  */
-    implicit val addressTypeReads: Reads[AddressType] = (
-      (JsPath \ "male").read[male] or
-      (JsPath \ "female").read[female]
-  )(AddressType.apply _)
-  
-  
- implicit val addressTypeWrites: Writes[AddressType] = (
-      (JsPath \ "male").read[male] or
-      (JsPath \ "female").read[female]
-     )(unlift(Person.unapply))
+    
+    def stringValueOf(value: AddressType): String = value match{
+        case Professional => "professional"
+        case Personal => "personal"
+        case _ => throw new IllegalArgumentException()
+    }
+    
+    implicit val addressTypeReads: Reads[AddressType] = 
+      __.read[String].map(AddressType.valueOf)
+    
+    implicit val addressTypeWrites = Writes[AddressType]( addressType =>
+        JsString(stringValueOf(addressType))
+    )
 }
 
+
+
 object  Sex{
-  //TODO hint : créer le Json Read et Write (ou Format)
-  implicit val sexFormat = new Format[Sex] {
-   def Writes(sex: Sex) = sex match{
-        case male => JsObject(Seq(
-            "sex" -> JsString("male")
-            ))
-        case female => JsObject(Seq(
-            "sex" -> JsString("female")
-            ))
-   }
-    
-    def Reads(jsonSex: JsValue) = {
-        val sex = (jsonSex \ "sex").as[String]
-        sex match {
-            case "male" => male
-            case "female" => female
-        }
+    //TODO hint : créer le Json Read et Write (ou Format)
+    def valueOf(value: String): Sex = value match{
+        case "male" => Male
+        case "female" => Female
+        case _ => throw new IllegalArgumentException()
     }
-  }
+    
+    def stringValueOf(value: Sex): String = value match{
+        case Male => "male"
+        case Female => "female"
+        case _ => throw new IllegalArgumentException()
+    }
+    
+    implicit val sexReads: Reads[Sex] = 
+    __.read[String].map(Sex.valueOf)
+    
+    implicit val sexWrites = Writes[Sex]( sex =>
+        JsString(stringValueOf(sex))
+    )
+  
 }
+
+
 
 object Person{
   //TODO hint : créer le Json Read et Write (ou Format)
-  import Sex._
-  import AddressType._
-  import Address._
   
-  //Here is my own custom Map Format:
   
-  implicit val myMap = new Format[Map[AddressType, Address]]{
-      def writeMyMap(map : Map[AddressType, Address]) = {
-          val mySeq = Seq()
-          map.foreach(
-              (tuple : (AddressType, Address))  =>
-              mySeq :+ Writes[tuple._1] :+ Writes[tuple._2]
+  //Here I make my own Map[AddressType, Address] reads and writes:
+    implicit val addrMapWrites = new Writes[Map[AddressType, Address]] {
+     def writes(addrMap: Map[AddressType, Address]) = {
+       val mySeq = Seq()
+          addrMap foreach(
+              tuple =>
+              mySeq :+ "addressType" -> Json.toJson(tuple._1) :+ "address" -> Json.toJson(tuple._2)
               )
           JsObject(mySeq)
-      }
-      
-      def readMyMap(jsonMyMap: JsValue) = {
-        //TO DO
-      }
-      
-      
-      
-  }
+     }
+    }
 
-
-     
+    implicit val addrMapReads = new Reads[Map[AddressType, Address]] {
+        def reads(jsvalue: JsValue): JsResult[Map[AddressType, Address]] = {
+            val mapper = new ObjectMapper() with ScalaObjectMapper
+            mapper.registerModule(DefaultScalaModule)
+            val mapData = mapper.readValue(jsonContent, classOf[Map[AddressType, Address]])   
+        
+        }
+    }
     
     
   implicit val personReads: Reads[Person] = (
@@ -123,13 +120,15 @@ object Person{
   )(Person.apply _)
   
   
- implicit val personWrites: Writes[Person] = (
-      (JsPath \ "name").write[String] and
-      (JsPath \ "lastName").write[String] and
-      (JsPath \ "birthDate").write[DateTime] and
-      (JsPath \ "sex").write[Sex] and
-      (JsPath \ "addresses").write[Map[AddressType, Address]]
-     )(unlift(Person.unapply))
+ implicit val personWrites = new Writes[Person] {
+     def writes(person: Person) = Json.obj(
+         "name" -> person.name,
+         "lastName" -> person.lastName,
+         "birthDate" -> person.birthDate,
+         "sex" -> person.sex,
+         "addresses" -> person.addresses
+         )
+ }
   /*
   implicit val personFormat = new Format[Person] {
       def writes(person: Person) = {
